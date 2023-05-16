9c25a873ce086d1310ad481340ea3486bfb06166
M1nKyu <124333357+M1nKyu@users.noreply.github.com>
2023-05-16 12:43:55 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+
+dp = pd.DataFrame(np.random.randn(500,2) / [50,50] + [37.76, -122.4], columns = ['lat', 'lon'])
+st.map(df)
+
 x = st.slider('Select a value') # 슬라이더 추가
 st.write(x, 'sqaured is', x * x) # 웹페이지를 텍스트처럼 사용할 수 있음
