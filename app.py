fa11dbb3109f690b504641262f7b72927e51dfb8
M1nKyu <124333357+M1nKyu@users.noreply.github.com>
2023-05-16 12:54:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,13 +10,13 @@ ax.hist(rand, bins=15)
 st.pyplot(fig)
 
 
-st.graphviz_chart(
-  digraph {
-    Big_shark -> Tuna
-    Tuna -> Mackerel
-    Mackerel -> small_fishes
-  }
-)
+st.graphviz_chart('''
+   digraph {
+      Big_shark -> Tuna
+      Tuna -> Mackerel
+      Mackerel -> small_fishes
+    }
+''')
 
 
 df = pd.DataFrame(np.random.randn(500,2) / [50,50] + [37.76, -122.4], columns = ['lat', 'lon'])
