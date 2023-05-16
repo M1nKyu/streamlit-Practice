51e636194359d35da20f1b3759252c6070aca926
M1nKyu <124333357+M1nKyu@users.noreply.github.com>
2023-05-16 12:53:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,14 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt # matplot 라이브러리가 없어서 오류가 발생할 것임, 설치필요
+import graphviz as graphviz
 
 rand = np.random.normal(1, 2, size = 20)
 fig, ax = plt.subplots()
 ax.hist(rand, bins=15)
 st.pyplot(fig)
 
-'''
+
 st.graphviz_chart(
   digraph {
     Big_shark -> Tuna
@@ -16,7 +17,7 @@ st.graphviz_chart(
     Mackerel -> small_fishes
   }
 )
-'''
+
 
 df = pd.DataFrame(np.random.randn(500,2) / [50,50] + [37.76, -122.4], columns = ['lat', 'lon'])
 st.map(df)
